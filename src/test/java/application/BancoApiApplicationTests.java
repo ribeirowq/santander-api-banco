c42b34f2d6cdaef039af1dc8640a93dd06bce2da
Thiago Lima <Ribeiro7380@gmail.com>
2025-07-02 19:55:25 -0400

    first commit

---
+++ src/test/java/application/BancoApiApplicationTests.java
@@ -0,0 +1,13 @@
+package application;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BancoApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
